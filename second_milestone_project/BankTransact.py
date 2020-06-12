a9191e43d4318885d9b78a485ffde1ce0ef41749
Sripad-jk <kulkarni.sripad@outlook.com>
2020-06-12 22:10:54 +0200

    This file keeps track of bank transaction

---
+++ second_milestone_project/BankTransact.py
@@ -0,0 +1,28 @@
+class BankRoll():
+    '''
+    This class maintains the users credit balance account
+    '''
+    
+    def __init__(self, amount=500):
+        '''
+        Initializes the bank roll by depositing default currency of 500
+        '''
+        self.amount = amount
+    
+    def withdraw(self, debit):
+        '''
+        Withdraws money of the said amount
+        If no funds, returns 'Not enough funds'
+        '''
+        if(self.amount >= debit):
+            self.amount = self.amount - debit
+            print(f'Current Balance = {self.amount}')
+        else:
+            return('Not enough funds')
+    
+    def deposit(self, credit):
+        '''
+        The routine adds the money to the account
+        '''
+        self.amount = self.amount + credit
+        print(f'Current Balance = {self.amount}')
